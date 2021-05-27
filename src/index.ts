36736de0d30de41d3ca636bbc9041d8bea1553c9
ldslayer <bibincito@gmail.com>
2021-05-27 08:49:23 -0400

    Correccion Base64

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@
 
 // LZW-compress a string
 function plot(s: string): string {
-    var dict: any;
+    var dict:any = {};
     var data = (s + "").split("");
     var out = [];
     var currChar;
@@ -33,7 +33,7 @@ function plot(s: string): string {
 
 // Decompress an LZW-encoded string
 function unplot(s: string): string {
-    var dict: any;
+    var dict: any = {};
     var data = (s + "").split("");
     var currChar = data[0];
     var oldPhrase = currChar;
@@ -85,45 +85,55 @@ var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
     b64re = /^(?:[A-Za-z\d+\/]{4})*?(?:[A-Za-z\d+\/]{2}(?:==)?|[A-Za-z\d+\/]{3}=?)?$/;
 
 const toB64 = function (string: string): string {
-    string = String(string);
-    var bitmap, a, b, c,
-        result = "", i = 0,
-        rest = string.length % 3; // To determine the final padding
+    // string = String(string);
+    // var bitmap, a, b, c,
+    //     result = "", i = 0,
+    //     rest = string.length % 3; // To determine the final padding
 
-    for (; i < string.length;) {
-        if ((a = string.charCodeAt(i++)) > 255
-            || (b = string.charCodeAt(i++)) > 255
-            || (c = string.charCodeAt(i++)) > 255)
-            throw new TypeError("Failed to execute 'btoa' on 'Window': The string to be encoded contains characters outside of the Latin1 range.");
+    // for (; i < string.length;) {
+    //     if ((a = string.charCodeAt(i++)) > 255
+    //         || (b = string.charCodeAt(i++)) > 255
+    //         || (c = string.charCodeAt(i++)) > 255)
+    //         throw new TypeError("Failed to execute 'btoa' on 'Window': The string to be encoded contains characters outside of the Latin1 range.");
 
-        bitmap = (a << 16) | (b << 8) | c;
-        result += b64.charAt(bitmap >> 18 & 63) + b64.charAt(bitmap >> 12 & 63)
-            + b64.charAt(bitmap >> 6 & 63) + b64.charAt(bitmap & 63);
+    //     bitmap = (a << 16) | (b << 8) | c;
+    //     result += b64.charAt(bitmap >> 18 & 63) + b64.charAt(bitmap >> 12 & 63)
+    //         + b64.charAt(bitmap >> 6 & 63) + b64.charAt(bitmap & 63);
+    // }
+
+    // // If there's need of padding, replace the last 'A's with equal signs
+    // return rest ? result.slice(0, rest - 3) + "===".substring(rest) : result;
+    if (window) {
+        return window.btoa(string)
+    } else {
+        return Buffer.from(string).toString('base64')
     }
-
-    // If there's need of padding, replace the last 'A's with equal signs
-    return rest ? result.slice(0, rest - 3) + "===".substring(rest) : result;
 };
 
 const B642a = function (string: string): string {
-    // atob can work with strings with whitespaces, even inside the encoded part,
-    // but only \t, \n, \f, \r and ' ', which can be stripped.
-    string = String(string).replace(/[\t\n\f\r ]+/g, "");
-    if (!b64re.test(string))
-        throw new TypeError("Failed to execute 'atob' on 'Window': The string to be decoded is not correctly encoded.");
+    // // atob can work with strings with whitespaces, even inside the encoded part,
+    // // but only \t, \n, \f, \r and ' ', which can be stripped.
+    // string = String(string).replace(/[\t\n\f\r ]+/g, "");
+    // if (!b64re.test(string))
+    //     throw new TypeError("Failed to execute 'atob' on 'Window': The string to be decoded is not correctly encoded.");
 
-    // Adding the padding if missing, for semplicity
-    string += "==".slice(2 - (string.length & 3));
-    var bitmap, result = "", r1, r2, i = 0;
-    for (; i < string.length;) {
-        bitmap = b64.indexOf(string.charAt(i++)) << 18 | b64.indexOf(string.charAt(i++)) << 12
-            | (r1 = b64.indexOf(string.charAt(i++))) << 6 | (r2 = b64.indexOf(string.charAt(i++)));
+    // // Adding the padding if missing, for semplicity
+    // string += "==".slice(2 - (string.length & 3));
+    // var bitmap, result = "", r1, r2, i = 0;
+    // for (; i < string.length;) {
+    //     bitmap = b64.indexOf(string.charAt(i++)) << 18 | b64.indexOf(string.charAt(i++)) << 12
+    //         | (r1 = b64.indexOf(string.charAt(i++))) << 6 | (r2 = b64.indexOf(string.charAt(i++)));
 
-        result += r1 === 64 ? String.fromCharCode(bitmap >> 16 & 255)
-            : r2 === 64 ? String.fromCharCode(bitmap >> 16 & 255, bitmap >> 8 & 255)
-                : String.fromCharCode(bitmap >> 16 & 255, bitmap >> 8 & 255, bitmap & 255);
+    //     result += r1 === 64 ? String.fromCharCode(bitmap >> 16 & 255)
+    //         : r2 === 64 ? String.fromCharCode(bitmap >> 16 & 255, bitmap >> 8 & 255)
+    //             : String.fromCharCode(bitmap >> 16 & 255, bitmap >> 8 & 255, bitmap & 255);
+    // }
+    // return result;
+    if (window) {
+        return window.atob(string)
+    } else {
+        return Buffer.from(string, 'base64').toString('ascii')
     }
-    return result;
 };
 
 const toEval = (arg: any) => eval(`${arg}`)
